c6e5fb19f7e09afecb8029944e45d58eb0be9094
Anannya Singh <anannya.singh@salesforce.com>
2026-05-02 16:51:07 +0530

    Add Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
-// TODO: Replace with your Firebase config
-// Get this from Firebase Console > Project Settings > Your apps > SDK setup and configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA4aqmcdjKTAgtIdVqXc_VxUilV-UtfYzA",
+  authDomain: "dsa-tracker-react-bda9f.firebaseapp.com",
+  databaseURL: "https://dsa-tracker-react-bda9f-default-rtdb.firebaseio.com",
+  projectId: "dsa-tracker-react-bda9f",
+  storageBucket: "dsa-tracker-react-bda9f.firebasestorage.app",
+  messagingSenderId: "306960975498",
+  appId: "1:306960975498:web:3bb77045cde85a82d84f73",
+  measurementId: "G-3M6K18N5DH"
 };
 
 const app = initializeApp(firebaseConfig);
